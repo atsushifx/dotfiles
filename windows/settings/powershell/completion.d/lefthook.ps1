f6cf6601c908461fc964b311494572413bb9492e
Furukawa Atsushi <atsushifx@gmail.com>
2025-06-09 17:37:11 +0900

    chore: integrate CodeGPT for commit message automation
    - .gitignore: Add .env files to the ignore list
    - lefthook.yml: Add a command for preparing commit messages using CodeGPT
    - .codegpt.yaml: Introduce configuration for CodeGPT with OpenAI settings
    - conventional_commit.tmpl: Create a template for generating conventional commits
    - summarize_file_diff.tmpl: Create a template for summarizing git diffs
    - summarize_title.tmpl: Create a template for writing pull request titles
    - project.dic: Remove custom dictionary file for spell checking
    - user.dic: Update user dictionary with new terms
    - lefthook.ps1: Add PowerShell script for Lefthook completion

---
+++ windows/settings/powershell/completion.d/lefthook.ps1
@@ -0,0 +1,9 @@
+# src: Documentation/powershell/completion.d/lefthook.ps1
+# @(#) : lefthook completion for powershell
+#
+# Copyright (c) 2025 Furukawa Atsushi <atsushifx@gmail.com>
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+lefthook completion powershell | Out-String | Invoke-Expression
