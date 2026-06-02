19d3b0b2b110ec25aec86634cc46b6f2dc29d223
Furukawa Atsushi <atsushifx@gmail.com>
2026-06-03 03:33:29 +0900

    config(commitlint): update parser preset and header length limit
    - configs/commitlint.config.js:
      `module.exports` 形式に変更（ESM `export default` を削除）
      `parserPreset.parserOpts.headerPattern` を追加してマージコミット形式をサポート
      `headerCorrespondence` に merge/pr フィールドを追加
      `header-max-length` を 72 から 76 に変更
      不要な TypeScript type check コメントを削除
    - configs/secretlint.config.yaml:
      著作権表記を `2025` から `2025-` に更新

--- configs/commitlint.config.js
+++ configs/commitlint.config.js
@@ -1,16 +1,26 @@
 // src: configs/commitlint.config.js
-// @(#) : commitlint basic configs
+// @(#) : commitlint basic configuration
 //
-// Copyright (c) 2025 atsushifx <http://github.com/atsushifx>
+// Copyright (c) 2025- atsushifx <http://github.com/atsushifx>
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// type check for typescript
-
 // commit lint common configs
-const baseConfig = {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?:(merge) \(#(\d+)\): )?(\w*)(?:\(([^)]*)\))?!?: (.+)$/,
+      headerCorrespondence: [
+        'merge',
+        'pr',
+        'type',
+        'scope',
+        'subject',
+      ],
+    },
+  },
   rules: {
     'type-enum': [2, 'always', [
       // === Default conventional types ===
@@ -32,9 +42,6 @@ const baseConfig = {
       'deps', // (custom) Updating third-party dependencies (npm/yarn/etc.)
     ]],
     'subject-case': [2, 'never', ['start-case', 'pascal-case']], // etc
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 76],
   },
 };
-
-// export
-export default baseConfig;
